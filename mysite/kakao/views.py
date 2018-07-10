d2162ca23a9c45d34e6b3a997709d64afc2baa09
YongBeomKim <saltman21@naver.com>
2018-07-10 17:42:33 +0900

    kakao-api

--- mysite/kakao/views.py
+++ mysite/kakao/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Kakao 챗봇을 위한 API 입니다.</h1>")
+
+def keyboard(request):
+    return JsonResponse({
+        'type':'buttons',
+        'buttons':['장고', '카톡', '시간'],
+    })
+
+@csrf_exempt
+def answer(request):
+    content = {
+        'message':{
+            'text':'공사중 입니다',
+            },
+        'keyboard':{
+            'type':'buttons',
+            'buttons':['장고', '카톡', '시간'],
+            }
+        }
+    return JsonResponse(content)
+
